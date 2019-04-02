a1bb0e40a607c86864861e0bf9dbda07cfb0602f
s17001 <s17001@std.it-college.ac.jp>
2019-04-02 20:16:25 +0900

    変数宣言でvarを使ってる箇所を滅ぼした

--- index.js
+++ index.js
@@ -1,130 +1,128 @@
+const canvas = document.querySelector("#scene")
+const ctx = canvas.getContext("2d")
+let particles = []
+let	amount = 0
+let mouse = {x:0,y:0}
+let radius = 1
+let colors = ["#468966","#FFF0A5", "#3affca"];
+
+let copy = document.querySelector("#sunaba");
+
+let ww = canvas.width = window.innerWidth;
+let wh = canvas.height = window.innerHeight;
+
+function Particle(x,y){
+  this.x =  Math.random()*ww;
+  this.y =  Math.random()*wh;
+  this.dest = {
+    x : x,
+    y: y
+  };
+  this.r =  Math.random()*0.8 + 1;
+  this.vx = (Math.random()-0.5)*20;
+  this.vy = (Math.random()-0.5)*20;
+  this.accX = 0;
+  this.accY = 0;
+  this.friction = Math.random()*0.05 + 0.94;
+
+  this.color = colors[Math.floor(Math.random()*3)];
+}
+
+Particle.prototype.render = function() {
 
 
-	var canvas = document.querySelector("#scene"),
-		ctx = canvas.getContext("2d"),
-		particles = [],
-		amount = 0,
-		mouse = {x:0,y:0},
-		radius = 1;
+  this.accX = (this.dest.x - this.x)/1000;
+  this.accY = (this.dest.y - this.y)/500;
+  this.vx += this.accX;
+  this.vy += this.accY;
+  this.vx *= this.friction;
+  this.vy *= this.friction;
 
-	var colors = ["#468966","#FFF0A5", "#3affca"];
+  this.x += this.vx;
+  this.y +=  this.vy;
 
-var copy = document.querySelector("#sunaba");
+  ctx.fillStyle = this.color;
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.r, Math.PI * 2, false);
+  ctx.fill();
 
-	var ww = canvas.width = window.innerWidth;
-	var wh = canvas.height = window.innerHeight;
+  let a = this.x - mouse.x;
+  let b = this.y - mouse.y;
 
-	function Particle(x,y){
-		this.x =  Math.random()*ww;
-		this.y =  Math.random()*wh;
-		this.dest = {
-			x : x,
-			y: y
-		};
-		this.r =  Math.random()*0.8 + 1;
-		this.vx = (Math.random()-0.5)*20;
-		this.vy = (Math.random()-0.5)*20;
-		this.accX = 0;
-		this.accY = 0;
-		this.friction = Math.random()*0.05 + 0.94;
+  let distance = Math.sqrt( a*a + b*b );
 
-		this.color = colors[Math.floor(Math.random()*3)];
-	}
+  if(distance<(radius*70)){
+    this.accX = (this.x - mouse.x)/100;
+    this.accY = (this.y - mouse.y)/10;
+    this.vx += this.accX;
+    this.vy += this.accY;
+  }
 
-	Particle.prototype.render = function() {
+}
 
+function onMouseMove(e){
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+}
 
-		this.accX = (this.dest.x - this.x)/1000;
-		this.accY = (this.dest.y - this.y)/500;
-		this.vx += this.accX;
-		this.vy += this.accY;
-		this.vx *= this.friction;
-		this.vy *= this.friction;
-
-		this.x += this.vx;
-		this.y +=  this.vy;
-
-		ctx.fillStyle = this.color;
-		ctx.beginPath();
-		ctx.arc(this.x, this.y, this.r, Math.PI * 2, false);
-		ctx.fill();
-
-		var a = this.x - mouse.x;
-		var b = this.y - mouse.y;
-
-		var distance = Math.sqrt( a*a + b*b );
-		if(distance<(radius*70)){
-			this.accX = (this.x - mouse.x)/100;
-			this.accY = (this.y - mouse.y)/10;
-			this.vx += this.accX;
-			this.vy += this.accY;
-		}
-
-	}
-
-	function onMouseMove(e){
-		mouse.x = e.clientX;
-		mouse.y = e.clientY;
-	}
-
-	function onTouchMove(e){
-    if(e.touches.length > 0 ){
-      mouse.x = e.touches[0].clientX;
-      mouse.y = e.touches[0].clientY;
-    }
-	}
+function onTouchMove(e){
+  if(e.touches.length > 0 ){
+    mouse.x = e.touches[0].clientX;
+    mouse.y = e.touches[0].clientY;
+  }
+}
 
 function onTouchEnd(e){
   mouse.x = -9999;
   mouse.y = -9999;
 }
 
-	function initScene(){
-		ww = canvas.width = window.innerWidth;
-		wh = canvas.height = window.innerHeight;
+function initScene(){
+  ww = canvas.width = window.innerWidth;
+  wh = canvas.height = window.innerHeight;
 
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-		ctx.font = "bold "+(ww/10)+"px sans-serif";
-		ctx.textAlign = "center";
-		ctx.fillText(copy.value, ww/2, wh/2);
+  ctx.font = "bold "+(ww/10)+"px sans-serif";
+  ctx.textAlign = "center";
+  ctx.fillText(copy.value, ww/2, wh/2);
 
-		var data  = ctx.getImageData(0, 0, ww, wh).data;
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.globalCompositeOperation = "screen";
+  let data  = ctx.getImageData(0, 0, ww, wh).data;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.globalCompositeOperation = "screen";
 
-		particles = [];
-		for(var i=0;i<ww;i+=Math.round(ww/600)){
-			for(var j=0;j<wh;j+=Math.round(ww/600)){
-				if(data[ ((i + j*ww)*4) + 3] > 150){
-					particles.push(new Particle(i,j));
-				}
-			}
-		}
-		amount = particles.length;
+  particles = [];
+  for(let i=0;i<ww;i+=Math.round(ww/600)){
+    for(let j=0;j<wh;j+=Math.round(ww/600)){
+      if(data[ ((i + j*ww)*4) + 3] > 150){
+        particles.push(new Particle(i,j));
+      }
+    }
+  }
+  amount = particles.length;
 
-	}
+}
 
-	function onMouseClick(){
-		radius++;
-		if(radius ===5){
-			radius = 0;
-		}
-	}
+function onMouseClick(){
+  radius++;
+  if(radius ===5){
+    radius = 0;
+  }
+}
 
-	function render(a) {
-		requestAnimationFrame(render);
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		for (var i = 0; i < amount; i++) {
-			particles[i].render();
-		}
-	};
+function render(a) {
+  requestAnimationFrame(render);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < amount; i++) {
+    particles[i].render();
+  }
+};
 
-	copy.addEventListener("keyup", initScene);
-	window.addEventListener("resize", initScene);
-	window.addEventListener("mousemove", onMouseMove);
-	window.addEventListener("touchmove", onTouchMove);
-	window.addEventListener("click", onMouseClick);
-	window.addEventListener("touchend", onTouchEnd);
-	initScene();
-	requestAnimationFrame(render);
+copy.addEventListener("keyup", initScene);
+window.addEventListener("resize", initScene);
+window.addEventListener("mousemove", onMouseMove);
+window.addEventListener("touchmove", onTouchMove);
+window.addEventListener("click", onMouseClick);
+window.addEventListener("touchend", onTouchEnd);
+initScene();
+requestAnimationFrame(render);
